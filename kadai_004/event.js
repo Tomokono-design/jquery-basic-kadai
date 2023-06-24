f27c1664fbf2e708e5f1aa823d3117a647598c10
Tomoko Kono <137596113+Tomokono-design@users.noreply.github.com>
2023-06-24 16:55:36 +0900

    kadai_004 提出

---
+++ kadai_004/event.js
@@ -0,0 +1,10 @@
+$(function() {
+  // HTMLドキュメントが読み込み完了したとき
+  $(window).on('load', () => {
+    console.log('loadイベントが発生しました');
+  });
+  // 画面をスクロールしたとき
+  $(window).on('scroll', () => {
+    console.log('scrollイベントが発生しました');
+  });
+});
