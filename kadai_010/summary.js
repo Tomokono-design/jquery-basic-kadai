3c35d1f9f126dcc91e68d78655f94e61852758d2
Tomoko Kono <137596113+Tomokono-design@users.noreply.github.com>
2023-06-25 16:18:46 +0900

    kadai_010 提出

---
+++ kadai_010/summary.js
@@ -0,0 +1,22 @@
+$(function() {
+  // 文字色変化
+  $('#change-color').on('click', () => {
+    $('#target').css('color', 'red');
+  });
+
+  // 文字内容変化
+  $('#change-text').on('click', () => {
+    $('#target').text('Good afternoon!');
+  });
+
+  // フェードアウト
+  $('#fade-out').on('click', () => {
+    $('#target').fadeOut();
+  });
+
+  // フェードイン
+  $('#fade-in').on('click', () => {
+    $('#target').fadeIn();
+  });
+
+});
